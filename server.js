83b255e0dee379e462f0c9084c2becfe750d28f7
indra <230414024.lpkia.ac.id>
2025-10-22 22:52:01 +0700

    second commit

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const port = 3000;
 
 app.use(express.json());
 
-// Data sementara (dummy)
+// Data dummy
 let users = [
-  { id: 1, name: 'Andi Aryanto', email: 'andi@example.com' },
+  { id: 1, name: 'Andri', email: 'andri@example.com' },
   { id: 2, name: 'Hakim', email: 'hakim@example.com' },
   { id: 3, name: 'Najmi', email: 'najmi@example.com' }
 ];
 
-// GET – tampilkan semua user
+// GET
 app.get('/users', (req, res) => {
   res.json(users);
 });
 
-// POST – tambah user baru
+// POST
 app.post('/users', (req, res) => {
   const newUser = {
     id: users.length + 1,
@@ -27,7 +27,7 @@ app.post('/users', (req, res) => {
   res.json({ message: 'User berhasil ditambahkan!', user: newUser });
 });
 
-// PUT – update seluruh data user
+// PUT
 app.put('/users/:id', (req, res) => {
   const id = parseInt(req.params.id);
   const user = users.find(u => u.id === id);
@@ -36,28 +36,6 @@ app.put('/users/:id', (req, res) => {
   user.name = req.body.name;
   user.email = req.body.email;
   res.json({ message: 'User berhasil diupdate!', user });
-});
-
-// PATCH – update sebagian data user
-app.patch('/users/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const user = users.find(u => u.id === id);
-  if (!user) return res.status(404).json({ message: 'User tidak ditemukan!' });
-
-  if (req.body.name) user.name = req.body.name;
-  if (req.body.email) user.email = req.body.email;
-
-  res.json({ message: 'User berhasil diupdate sebagian!', user });
-});
-
-// DELETE – hapus user
-app.delete('/users/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const index = users.findIndex(u => u.id === id);
-  if (index === -1) return res.status(404).json({ message: 'User tidak ditemukan!' });
-
-  users.splice(index, 1);
-  res.json({ message: 'User berhasil dihapus!' });
 });
 
 app.listen(port, () => {
